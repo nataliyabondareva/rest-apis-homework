6894b35eb44632f2f2f6d6d52325b51c2695072d
tallybee <talbondareva@gmail.com>
2019-05-25 10:31:12 +0200

    first step complete and fixed

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Express API listening on port 3000'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => res.redirect("/messages"));
+app.get("/messages", (req, res) => res.json({ data: messages }));
+let requestsCount = 0;
+app.post("/messages", (req, res) => {
+  if (req.body.text && requestsCount < 6) {
+    requestsCount = +1;
+    console.log("reqconnt", requestsCount);
+    console.log(req.body.text);
+  } else if (!req.body.text && requestsCount < 6) {
+    console.log("reqconnt", requestsCount);
+    requestsCount = +1;
+    res.status(400).json({
+      message: "Oh no! 400: Bad Request"
+    });
+  } else if (requestsCount >= 6) {
+    console.log("reqconnt", requestsCount);
+    console.log("stop!");
+    res.status(500).json({
+      message: "Whoops! Internal server error"
+    });
+  }
+});
+app.listen(port, () => `Listening on port ${port}`);
